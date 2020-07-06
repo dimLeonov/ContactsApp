35ca0daf1e12540b81a6809a21cd2aed408c9325
Dmitry <dim.leonov97@gmail.com>
2020-07-06 23:42:38 +0300

    Initial commit

---
+++ Contacts/task/src/contacts/Main.java
@@ -0,0 +1,10 @@
+package contacts;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        ContactMethods contactMethods = new ContactMethods();
+        contactMethods.enterTheNameOfThePerson();
+    }
+}
